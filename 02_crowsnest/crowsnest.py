f37b2e92aaf53527c93befdf15dbaa06de518f87
nate-orage <nate54o@icloud.com>
2021-07-23 09:03:33 +0000

    nothing

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -37,20 +37,5 @@ def main():
         article = "a"
     print(f"Ahoy, Captain, {article} {word} off the larboard bow!")
 
-    # print('narwhal')
-    # str_arg = args.arg
-    # int_arg = args.int
-    # file_arg = args.file
-    # flag_arg = args.on
-    # pos_arg = args.positional
-
-    # print(f'str_arg = "{str_arg}"')
-    # print(f'int_arg = "{int_arg}"')
-    # print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    # print(f'flag_arg = "{flag_arg}"')
-    # print(f'positional = "{pos_arg}"')
-
-
-# --------------------------------------------------
 if __name__ == "__main__":
     main()
