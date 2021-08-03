23afc87546d2c5317f2a8ef0ba61e5890e662447
nate-orage <nate54o@icloud.com>
2021-08-03 01:20:20 +0000

    force add jump.py

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,55 @@
+#!/home/nate/python/python_env/bin/python3
+"""
+Author : nate <nate@localhost>
+Date   : 2021-07-31
+Purpose: Jump the Five
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Jump the Five",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "text",
+        metavar="str",
+        type=str,
+        help="Each number in the text will be encoded using this algorithm.",
+    )
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    args = get_args()
+    text = (
+        args.text
+    )  # This creates a variable by extracting the values used by the argument
+    jumper = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+    }
+
+    new_text = ""
+    for char in text:
+        new_text += jumper.get(char, char)
+    print(new_text)
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
