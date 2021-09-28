7a1fc2fdbe8005261b4f1fe29a0b6cc1381ae736
nate-orage <nate54o@icloud.com>
2021-09-28 22:26:23 +0000

    cleanup

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -16,49 +16,53 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Howler',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Howler", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument('text',
-                        metavar='str',
-                        help='Input Text Here')
+    parser.add_argument("text", metavar="str", help="Input Text Here")
 
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='This will output to a text file.',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="This will output to a text file.",
+        metavar="str",
+        type=str,
+        default="",
+    )
 
     args = parser.parse_args()
-    if os.path.isfile(args.text): # If the argument given is a text file, the program will open it to read.
+    if os.path.isfile(
+        args.text
+    ):  # If the argument given is a text file, the program will open it to read.
         args.text = open(args.text)
     else:
-        args.text = io.StringIO(args.text + '\n')
-    
+        args.text = io.StringIO(args.text + "\n")
+
     return args
+
+
 # --------------------------------------------------
 def main():
     args = get_args()
-    '''One Solution'''
+    """One Solution"""
     # if args.outfile:
     #     print(args.text.upper(),file=open(args.outfile, 'wt'))
     # else:
     #     print(args.text.upper())
-    '''Another Solution'''
+    """Another Solution"""
     # out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
     # print(args.text.upper(), file=out_fh)
-    '''Another Solution'''
+    """Another Solution"""
     # out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
     # out_fh.write(args.text.upper() + '\n')
     # out_fh.close()
-    '''Solution to save memory'''
-    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    """Solution to save memory"""
+    out_fh = open(args.outfile, "wt") if args.outfile else sys.stdout
     for line in args.text:
         out_fh.write(line.upper())
     out_fh.close()
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
